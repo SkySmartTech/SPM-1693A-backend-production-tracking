ff2dfafd611a62e9744d31acc40b6a96a82476b2
Tharaka Prasad <129380063+tharaka-prasad@users.noreply.github.com>
2025-05-03 11:32:33 +0530

    `Refactored user authentication and management API endpoints`

--- app/Models/User.php
+++ app/Models/User.php
@@ -31,6 +31,10 @@ class User extends Authenticatable
         'status',
     ];
 
+    protected $casts = [
+        'availability' => 'boolean',
+        
+    ];
     /**
      * The attributes that should be hidden for serialization.
      *
