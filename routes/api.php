2b5a62401811e06af8f1e977872efac1231feb01
udayanagani <145590621+udayanagani@users.noreply.github.com>
2025-05-15 11:56:13 +0530

    api updated

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,11 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\Auth\UserRegisterController;
+use App\Http\Controllers\Color\ColorSettingController;
+use App\Http\Controllers\Defect\DefectController;
+use App\Http\Controllers\Operation\OperationController;
+use App\Http\Controllers\Size\SizeSettingController;
+use App\Http\Controllers\Style\StyleSettingController;
 use App\Http\Controllers\User\UserAccessController;
 use App\Http\Controllers\User\UserRoleController;
 use Illuminate\Support\Facades\Route;
@@ -32,5 +37,39 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('user-roles', [UserRoleController::class, 'index']);
     Route::post('user-role/{id}/update', [UserRoleController::class, 'update']);
     Route::get('user-role/{id}/delete', [UserRoleController::class, 'destroy']);
+
+    Route::post('color-create', [ColorSettingController::class, 'store']);
+    Route::get('all-colors', [ColorSettingController::class, 'index']);
+    Route::get('color/{id}/show', [ColorSettingController::class, 'show']);
+    Route::post('color/{id}/update', [ColorSettingController::class, 'update']);
+    Route::get('color/{id}/delete', [ColorSettingController::class, 'destroy']);
+
+
+    Route::post('size-create', [SizeSettingController::class, 'store']);
+    Route::get('all-sizes', [SizeSettingController::class, 'index']);
+    Route::get('size/{id}/show', [SizeSettingController::class, 'show']);
+    Route::post('size/{id}/update', [SizeSettingController::class, 'update']);
+    Route::get('size/{id}/delete', [SizeSettingController::class, 'destroy']);
+
+
+    Route::post('style-create', [StyleSettingController::class, 'store']);
+    Route::get('all-styles', [StyleSettingController::class, 'index']);
+    Route::get('style/{id}/show', [StyleSettingController::class, 'show']);
+    Route::post('style/{id}/update', [StyleSettingController::class, 'update']);
+    Route::get('style/{id}/delete', [StyleSettingController::class, 'destroy']);
+
+
+    Route::post('operation-create', [OperationController::class, 'store']);
+    Route::get('all-operations', [OperationController::class, 'index']);
+    Route::get('operation/{id}/show', [OperationController::class, 'show']);
+    Route::post('operation/{id}/update', [OperationController::class, 'update']);
+    Route::get('operation/{id}/delete', [OperationController::class, 'destroy']);
+
+
+    Route::post('defect-create', [DefectController::class, 'store']);
+    Route::get('all-defects', [DefectController::class, 'index']);
+    Route::get('defect/{id}/show', [DefectController::class, 'show']);
+    Route::post('defect/{id}/update', [DefectController::class, 'update']);
+    Route::get('defect/{id}/delete', [DefectController::class, 'destroy']);
 
 });
