b8a39abed07602f7da86767894057f3f1b55841b
udayanagani <145590621+udayanagani@users.noreply.github.com>
2025-05-20 14:33:41 +0530

    user get updated

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\User\UserProfileUpdateRequest;
 use App\Http\Requests\User\UserUpdateRequest;
 use App\Repositories\All\User\UserInterface;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -22,6 +23,51 @@ class UserController extends Controller
     {
         $users = $this->userInterface->all();
         return response()->json($users, 200);
+    }
+
+    public function show(Request $request)
+    {
+        $user = $request->user();
+
+        if (! $user || $user->availability != 1) {
+            return response()->json(['message' => 'User not available'], 403);
+        }
+
+        $permission = $this->comPermissionInterface->getById($user->userType);
+        $userData   = $user->toArray();
+
+        $profileImages = is_array($user->profileImage) ? $user->profileImage : json_decode($user->profileImage, true) ?? [];
+
+        $signedImages = [];
+        foreach ($profileImages as $uri) {
+            $signed         = $this->profileImageService->getImageUrl($uri);
+            $signedImages[] = [
+                'fileName' => $signed['fileName'] ?? null,
+                'imageUrl' => $signed['signedUrl'] ?? null,
+            ];
+        }
+
+        foreach ($profileImages as &$uri) {
+            if (isset($document['gsutil_uri'])) {
+                $imageData            = $this->profileImageService->getImageUrl($document['gsutil_uri']);
+                $document['imageUrl'] = $imageData['signedUrl'];
+                $document['fileName'] = $imageData['fileName'];
+            }
+        }
+
+        $userData['profileImage'] = $signedImages;
+
+        if ($permission) {
+            $userData['permissionObject'] = (array) $permission->permissionObject;
+            $userData['userTypeObject']   = [
+                'id'   => $permission->id,
+                'name' => $permission->userType ?? null,
+            ];
+        }
+
+        $userData['assigneeLevelObject'] = $this->assigneeLevelInterface->getById($user->assigneeLevel);
+
+        return response()->json($userData, 200);
     }
 
     public function update(UserUpdateRequest $request, $id)
--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ Route::middleware('auth:sanctum')->get('/user', [UserController::class, 'show'])
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('all-users', [UserController::class, 'index']);
+    Route::get('user', [UserController::class, 'show']);
     Route::put('user/{id}/update', [UserController::class, 'update']);
     Route::post('user/{id}/profile-update', [UserController::class, 'profile_update']);
 
