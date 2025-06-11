636026e36e78b54048133077b2d3e295b5e4fa4f
udayanagani <145590621+udayanganiariyasena@users.noreply.github.com>
2025-06-11 14:51:20 +0530

    bug fix qr_code -> QRCode

--- app/Http/Requests/ProductionUpdate/ProductionUpdateCreateRequest.php
+++ app/Http/Requests/ProductionUpdate/ProductionUpdateCreateRequest.php
@@ -22,22 +22,22 @@ class ProductionUpdateCreateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'serverDateTime' => 'required|date',
-            'lineNo'          => 'nullable|string|max:255',
-            'QRCode'          => 'nullable|string|max:255|unique:production_updates,qr_code',
-            'buyer'            => 'nullable|string|max:255',
-            'gg'               => 'nullable|string|max:255',
-            'smv'              => 'nullable|numeric',
-            'presentCarder'   => 'nullable|numeric',
-            'style'            => 'nullable|string|max:255',
-            'color'            => 'nullable|string|max:255',
-            'sizeName'        => 'nullable|string|max:255',
-            'checkPoint'      => 'nullable|string|max:255',
-            'qualityState'    => 'nullable|string|max:255',
-            'part'             => 'nullable|string|max:255',
-            'location'         => 'nullable|string|max:255',
-            'defectCode'      => 'nullable|string|max:255',
-            'state'            => 'nullable|integer',
+            'serverDateTime'    => 'required|date',
+            'lineNo'            => 'nullable|string|max:255',
+            'QRCode'            => 'nullable|string|max:255|unique:production_updates,QRCode',
+            'buyer'             => 'nullable|string|max:255',
+            'gg'                => 'nullable|string|max:255',
+            'smv'               => 'nullable|numeric',
+            'presentCarder'     => 'nullable|numeric',
+            'style'             => 'nullable|string|max:255',
+            'color'             => 'nullable|string|max:255',
+            'sizeName'          => 'nullable|string|max:255',
+            'checkPoint'        => 'nullable|string|max:255',
+            'qualityState'      => 'nullable|string|max:255',
+            'part'              => 'nullable|string|max:255',
+            'location'          => 'nullable|string|max:255',
+            'defectCode'        => 'nullable|string|max:255',
+            'state'             => 'nullable|integer',
         ];
     }
 }
