fb9b022f833746072d67fc431657250baa5e4832
udayanagani <145590621+udayanganiariyasena@users.noreply.github.com>
2025-07-02 16:29:01 +0530

    create Dashboard model

--- app/Models/Dashboard.php
+++ app/Models/Dashboard.php
@@ -24,6 +24,7 @@ class Dashboard extends Model
         'hourlyBalance',
         'totalCheckQuantity',
         'totalDefects',
+        'topDefectCode',
         'DHU',
         'performanceEFI',
         'lineEFI',
