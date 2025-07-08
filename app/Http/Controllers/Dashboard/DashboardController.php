610d3b5f5dafe1c8a84b7d33c7b315cc77079ef2
udayanagani <145590621+udayanganiariyasena@users.noreply.github.com>
2025-07-08 16:28:30 +0530

    Remove function consider working hours to calculate data

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -30,11 +30,11 @@ class DashboardController extends Controller
         $currentHourStart = $shiftStart->copy()->addHours($hoursSinceShiftStart);
         $currentHourEnd = $currentHourStart->copy()->addHour();
 
-        if ($now->lt($shiftStart) || $now->gt($shiftStart->copy()->addHours(9))) {
-            return response()->json([
-                'message' => 'Outside working hours. No dashboard data generated.'
-            ], 200);
-        }
+        // if ($now->lt($shiftStart) || $now->gt($shiftStart->copy()->addHours(9))) {
+        //     return response()->json([
+        //         'message' => 'Outside working hours. No dashboard data generated.'
+        //     ], 200);
+        // }
 
         $dayPlans = DB::table('day_plans')
                 ->whereDate('created_at', $today)
