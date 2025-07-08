3b8fc11e60afeb5e8f93863c14e20e639b22ec3d
udayanagani <145590621+udayanganiariyasena@users.noreply.github.com>
2025-07-08 16:24:05 +0530

    add new route to get day-plan's team-no

--- routes/api.php
+++ routes/api.php
@@ -88,7 +88,8 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('part-location/{id}/delete', [PartsLocationsController::class, 'destroy']);
 
     Route::post('day-plan-create', [DayPlanController::class, 'store']);
-    Route::get('all-day-plans', [DayPlanController::class, 'index']);
+    Route::get('all-day-plans', [DayPlanController::class, 'getLatestDayPlanSet']);
+    Route::get('day-plan/team-no', [DayPlanController::class, 'index']);
     Route::post('get-production-data', [DayPlanController::class, 'show']);
 
     Route::post('production-update', [ProductionUpdateController::class, 'store']);
